52f2f6f4a45035449ae7117a4214ab21427e22ec
Ronald-Him <85689119+Ronald-Him@users.noreply.github.com>
2023-10-03 15:57:57 +0700

    init

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,3 +1,46 @@
+'use client';
+import { useContext, useEffect, useState } from "react";
+import Image from "next/image";
+import { Link } from "react-scroll";
+import SearchMobile from "./SearchMobile";
+import { useMediaQuery } from "react-responsive";
+
+
+
+
 export default function Header() {
-    return <div>Header</div>;
+    const [header, setHeader] = useState(false);
+    const [nav, setNav] = useState(false);
+
+    const desktopMode = useMediaQuery({
+        query: '(min-width: 1300px)',
+    });
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 40) {
+                setHeader(true);
+            }
+            else {
+                setHeader(false);
+            }
+        };
+        //add event listener
+        window.addEventListener('scroll', handleScroll);
+
+        //remove event listener
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    });
+
+    console.log(header);
+
+
+
+    return (
+    <header className={`${header ? 'bg-white shadow-md py-2' : 'bg-transparent shadow-none py-4'} fixed w-full max-w-[1920px] mx-auto z-20 transition-all duration-300`}>
+        <div className="xl:container mx-auto">Header</div>
+    </header>
+    );
 }

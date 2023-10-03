bef92fe02ef35021d42613e32d1e91c34d0de660
Ronald-Him <85689119+Ronald-Him@users.noreply.github.com>
2023-10-03 16:14:39 +0700

    changes

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -40,7 +40,7 @@ export default function Header() {
 
     return (
     <header className={`${header ? 'bg-white shadow-md py-2' : 'bg-transparent shadow-none py-4'} fixed w-full max-w-[1920px] mx-auto z-20 transition-all duration-300`}>
-        <div className="xl:container mx-auto">Header</div>
+        <div className="xl:container mx-auto flex flex-col xl:flex-row xl:item-center xl:justify-between">Header</div>
     </header>
     );
 }
